7d8988dfcd6bdc7a664099dccc636123450396b1
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-12-03 12:42:40 -0300

    ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -17,8 +17,10 @@ class RemoteAuthentication {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
     try {
       await httpClient.request(url: url, method: 'post', body: body);
-    } on HttpError catch (e) {
-      throw DomainError.unexpected;
+    } on HttpError catch (error) {
+      throw error == HttpError.unauthorized
+          ? DomainError.invalidCredentials
+          : DomainError.unexpected;
     }
   }
 }
