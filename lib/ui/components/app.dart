464908b44d4d8fef4c4dd0105467c062a79eaa9e
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-12-07 11:02:44 -0300

    add LoginPage

---
+++ lib/ui/components/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../pages/pages.dart';
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+         title: '4Dev',
+         home:  LoginPage(),
+    );
+  }
+}
