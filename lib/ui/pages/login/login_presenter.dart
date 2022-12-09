9e4d47fad69c7f576256490d1b194b4a60950fc6
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-12-09 15:37:38 -0300

    ensure LoginPage present loading

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -5,6 +5,8 @@ abstract class LoginPresenter {
 
   Stream get isFormValidStream;
 
+  Stream get isLoadingController;
+
   void validateEmail(String email);
 
   void validatePassword(String password);
