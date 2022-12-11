8204bf3dabe78268945cc45f9d4a9bfc22e91a9b
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-12-11 01:45:45 -0300

    ensure StreamLoginPresenter emits passwordError as null if password is valid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -77,4 +77,15 @@ void main() {
     sut.validatePassword(password);
     sut.validatePassword(password);
   });
+
+  test('Should passwordError emit null if validation succeeds', () {
+    sut.passwordErrorStream
+        .listen(expectAsync1((error) => expect(error, null)));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validatePassword(password);
+    sut.validatePassword(password);
+  });
 }
