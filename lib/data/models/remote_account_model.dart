8a8fa07490a58109206dae5980643f24d0c8b776
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-12-21 12:25:47 -0300

    improve code quality

--- lib/data/models/remote_account_model.dart
+++ lib/data/models/remote_account_model.dart
@@ -1,16 +1,17 @@
+import 'package:meta/meta.dart';
 import '../../domain/entities/entities.dart';
 import '../http/http.dart';
 
 class RemoteAccountModel {
   final String accessToken;
 
-  RemoteAccountModel(this.accessToken);
+  RemoteAccountModel({@required this.accessToken});
 
   factory RemoteAccountModel.fromJson(Map json) {
     if (!json.containsKey('accessToken')) {
       throw HttpError.invalidData;
     }
-    return RemoteAccountModel(json['accessToken']);
+    return RemoteAccountModel(accessToken: json['accessToken']);
   }
 
   AccountEntity toEntity() => AccountEntity(token: accessToken);
