518d17a18c4f0baf77b996fa985a7e6777aa5bb5
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-12-24 19:48:00 -0300

    ensure CompareFieldsValidation return error if values are not equal

---
+++ test/validation/validators/compare_fields_validation_test.dart
@@ -0,0 +1,21 @@
+import 'package:test/test.dart';
+
+import 'package:fordev/presentation/protocols/validation.dart';
+
+import 'package:fordev/validation/validators/validators.dart';
+
+void main() {
+  CompareFieldsValidation sut;
+  setUp(() {
+    sut = CompareFieldsValidation(
+      field: 'any_field',
+      valueToCompare: 'any_value',
+    );
+  });
+
+  test('should return error if values are not equal', () async {
+    final differentValuesError = sut.validate('wrong_value');
+
+    expect(differentValuesError, ValidationError.invalidField);
+  });
+}
