6597717576505ce13a0232d334f72c155b3f71fd
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-12-24 02:32:24 -0300

    ensure GetxSignUpPresenter emits correct error on name changed

--- test/presentation/presenters/getx_signup_presenter_test.dart
+++ test/presentation/presenters/getx_signup_presenter_test.dart
@@ -12,6 +12,7 @@ void main() {
   GetxSignUpPresenter sut;
   Validation validation;
   String email;
+  String name;
 
   PostExpectation mockValidationCall(String field) => when(validation.validate(
         field: field == null ? anyNamed('field') : field,
@@ -29,6 +30,7 @@ void main() {
       validation: validation,
     );
     email = faker.internet.email();
+    name = faker.person.name();
     mockValidation();
   });
 
@@ -76,5 +78,51 @@ void main() {
 
     sut.validateEmail(email);
     sut.validateEmail(email);
+  });
+
+  test('Should call Validation with correct name', () {
+    sut.validateName(name);
+
+    verify(validation.validate(field: 'name', value: name)).called(1);
+  });
+
+  test('Should emit invalidFieldError if name is invalid', () {
+    mockValidation(field: 'name', value: ValidationError.invalidField); //test
+
+    sut.nameErrorStream.listen(
+      expectAsync1((error) => expect(error, UIError.invalidField)),
+    );
+
+    sut.isFormValidStream.listen(
+      expectAsync1((isValid) => expect(isValid, false)),
+    );
+
+    sut.validateName(name);
+    sut.validateName(name);
+  });
+
+  test('Should emit requiredFieldError if name is empty', () {
+    mockValidation(value: ValidationError.requiredField);
+
+    sut.nameErrorStream
+        .listen(expectAsync1((error) => expect(error, UIError.requiredField)));
+
+    sut.isFormValidStream.listen(
+      expectAsync1((isValid) => expect(isValid, false)),
+    );
+
+    sut.validateName(name);
+    sut.validateName(name);
+  });
+
+  test('Should emit null if name validation succeeds', () {
+    sut.nameErrorStream.listen(expectAsync1((error) => expect(error, null)));
+
+    sut.isFormValidStream.listen(
+      expectAsync1((isValid) => expect(isValid, false)),
+    );
+
+    sut.validateName(name);
+    sut.validateName(name);
   });
 }
