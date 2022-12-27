91c41dd3667161991ee9b31887ce32c1062f013d
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-12-27 15:41:34 -0300

    ensure HttpAdapter throws ServerError on 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -250,5 +250,13 @@ void main() {
 
       expect(future, throwsA(HttpError.notFound));
     });
+
+    test('should return ServerError if get returns 500', () async {
+      mockResponse(500);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
