c6a8e196c7a22ec7a62f97b5975c8133e927e3fc
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-12-27 14:32:14 -0300

    ensure RemoteLoadSurveys throws UnexpectedError on 500

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -134,4 +134,12 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('should throw UnexpectedError if HttpClient returns 500', () async {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
