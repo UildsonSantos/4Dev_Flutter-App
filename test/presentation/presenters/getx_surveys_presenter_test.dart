eb1551f683181a57a238320dbbb6e2b293bc9457
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-01-07 12:13:48 -0300

    ensure GetxSurveysPresenter emits correct events on success

--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -1,28 +1,100 @@
+import 'package:faker/faker.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-import 'package:fordev/domain/usecases/load_surveys.dart';
+import 'package:fordev/domain/entities/entities.dart';
+import 'package:fordev/domain/usecases/usecases.dart';
+
+import 'package:fordev/ui/pages/pages.dart';
 
 class GetxSurveysPresenter {
   final LoadSurveys loadSurveys;
 
+  final _isLoading = true.obs;
+  final _surveys = Rx<List<SurveyViewModel>>();
+
+  Stream<bool> get isLoadingStream => _isLoading.stream;
+  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
+
   GetxSurveysPresenter({@required this.loadSurveys});
 
   Future<void> loadData() async {
-    await loadSurveys.load();
+    _isLoading.value = true;
+    final surveys = await loadSurveys.load();
+    
+    _surveys.value = surveys.map(
+      (survey) => SurveyViewModel(
+        id: survey.id,
+        question: survey.question,
+        date: DateFormat('dd MMM yyyy').format(survey.dateTime),
+        didAnswer: survey.didAnswer,
+      ),
+    ).toList();
+
+    _isLoading.value = false;
   }
 }
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
 void main() {
-  test('should call loadSurveys on loadData', () async {
-    final loadSurveys = LoadSurveysSpy();
-    final sut = GetxSurveysPresenter(loadSurveys: loadSurveys);
+  GetxSurveysPresenter sut;
+  LoadSurveysSpy loadSurveys;
+  List<SurveyEntity> surveys;
 
+  List<SurveyEntity> mockValidData() => [
+        SurveyEntity(
+            id: faker.guid.guid(),
+            question: faker.lorem.sentence(),
+            dateTime: DateTime(2020, 2, 20),
+            didAnswer: true),
+        SurveyEntity(
+            id: faker.guid.guid(),
+            question: faker.lorem.sentence(),
+            dateTime: DateTime(2019, 6, 26),
+            didAnswer: false),
+      ];
+
+  void mockLoadSurveys(List<SurveyEntity> data) {
+    surveys = data;
+    when(loadSurveys.load()).thenAnswer((_) async => surveys);
+  }
+
+  setUp(() {
+    loadSurveys = LoadSurveysSpy();
+    sut = GetxSurveysPresenter(loadSurveys: loadSurveys);
+    mockLoadSurveys(mockValidData());
+  });
+
+  test('should call loadSurveys on loadData', () async {
     await sut.loadData();
 
     verify(loadSurveys.load()).called(1);
+  });
+
+  test('should emits correct events on success', () async {
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    sut.surveysStream.listen(
+      (surveys) => expect(surveys, [
+        SurveyViewModel(
+          id: surveys[0].id,
+          question: surveys[0].question,
+          date: '20 Fev 2020',
+          didAnswer: surveys[0].didAnswer,
+        ),
+        SurveyViewModel(
+          id: surveys[1].id,
+          question: surveys[1].question,
+          date: '03 Out 2018',
+          didAnswer: surveys[1].didAnswer,
+        ),
+      ]),
+    );
+
+    await sut.loadData();
   });
 }
