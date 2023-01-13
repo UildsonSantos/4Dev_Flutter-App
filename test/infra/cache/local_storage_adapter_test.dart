0c04dfc4318e1b1be3e561a23880dc7332fc823c
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-01-13 14:15:13 -0300

    ensure LocalStorageAdapter deletes cache with correct key

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -51,4 +51,12 @@ void main() {
       expect(future, throwsA(TypeMatcher<Exception>()));
     });
   });
+
+  group('delete', () {
+    test('should call localStorage with correct values', () async {
+      await sut.delete(key);
+
+      verify(localStorage.deleteItem(key)).called(1);
+    });
+  });
 }
