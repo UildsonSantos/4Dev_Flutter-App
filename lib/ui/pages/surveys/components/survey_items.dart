6e469808382be581de1d3a139ce36b6d1cfe6982
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-01-17 02:07:09 -0300

    move SurveyItems Component to its own file

---
+++ lib/ui/pages/surveys/components/survey_items.dart
@@ -0,0 +1,26 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import '../surveys.dart';
+import 'components.dart';
+
+class SurveyItems extends StatelessWidget {
+  final List<SurveyViewModel> viewModel;
+  const SurveyItems({@required this.viewModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: CarouselSlider(
+        options: CarouselOptions(
+          enlargeCenterPage: true,
+          aspectRatio: 1,
+        ),
+        items: viewModel
+            .map((viewModel) => SurveyItem(viewModel: viewModel))
+            .toList(),
+      ),
+    );
+  }
+}
--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,4 +1,4 @@
-import 'package:carousel_slider/carousel_slider.dart';
+
 import 'package:flutter/material.dart';
 
 import '../../components/components.dart';
@@ -38,17 +38,8 @@ class SurveysPage extends StatelessWidget {
               );
             }
             if (snapshot.hasData) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: CarouselSlider(
-                  options: CarouselOptions(
-                    enlargeCenterPage: true,
-                    aspectRatio: 1,
-                  ),
-                  items: snapshot.data
-                      .map((viewModel) => SurveyItem(viewModel: viewModel))
-                      .toList(),
-                ),
+              return SurveyItems(
+                viewModel: snapshot.data,
               );
             }
             return SizedBox(height: 0);
