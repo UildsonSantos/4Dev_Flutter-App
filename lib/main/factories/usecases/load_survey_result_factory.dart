45c0787c00d7adcd513e032a609b42b296f8e4e2
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-01-23 14:36:48 -0300

    add LoadSurveyResult composition

--- lib/main/factories/usecases/load_survey_result_factory.dart
+++ lib/main/factories/usecases/load_survey_result_factory.dart
@@ -1,9 +1,21 @@
 import '../../../data/usecases/usecases.dart';
 import '../../../domain/usecases/usecases.dart';
+import '../../composites/composites.dart';
 import '../factories.dart';
 
 LoadSurveyResult makeRemoteLoadSurveyResult(String surveyId) =>
     RemoteLoadSurveyResult(
       httpClient: makeAuthorizedHttpClientDecorator(),
       url: makeApiUrl('surveys/$surveyId/results'),
+    );
+
+LoadSurveyResult makeLocalLoadSurveyResult(String surveyId) =>
+    LocalLoadSurveyResult(
+      cacheStorage: makeLocalStorageAdapter(),
+    );
+
+LoadSurveyResult makeRemoteLoadSurveyResultWithLocalFallback(String surveyId) =>
+    RemoteLoadSurveyResultWithLocalFallback(
+      local: makeLocalLoadSurveyResult(surveyId),
+      remote: makeRemoteLoadSurveyResult(surveyId),
     );
