f8c71aead4bea50fb17a658715f0c37304356635
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-01-24 13:03:51 -0300

    add SaveSurveyResult use case

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,6 +1,7 @@
 export './add_account.dart';
 export './authentication.dart';
 export './load_current_account.dart';
+export './load_survey_result.dart';
 export './load_surveys.dart';
-export './load_surveys_result.dart';
 export './save_current_account.dart';
+export './save_survey_result.dart';
